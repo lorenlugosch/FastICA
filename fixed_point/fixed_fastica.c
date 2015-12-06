a81856adf6f69a25525ff591050e34bdaefbf269
Loren Lugosch <loren.lugosch@gmail.com>
2015-12-06 12:08:17 -0500

    removed function calls in fixed point version

--- fixed_point/fixed_fastica.c
+++ fixed_point/fixed_fastica.c
@@ -3,15 +3,19 @@
 #include <stdint.h>
 #include "fixed_fastica.h"
 
+Q21_43 sum_2;
+Q11_21 w_rnorm;
+Q11_21 dot_product;
+
 Q11_21 multiply_Q6_10_by_Q6_10(Q6_10 a, Q6_10 b) {
-	//if ((a == b) && (a == -65536)) { return 65535; }
+	if ((a == b) && (a == -32768)) { return 2147483647; }
 	Q11_21 cc = (Q11_21)a * (Q11_21)b * 2;
 
 	return cc;
 }
 
 Q21_43 multiply_Q11_21_by_Q11_21(Q11_21 a, Q11_21 b) {
-	//if ((a == b) && (a == -65536)) { return 65535; }
+	if ((a == b) && (a == -2147483648)) { return 9223372036854775807; }
 	Q21_43 cc = (Q21_43)a * (Q21_43)b * 2;
 
 	return cc;
@@ -35,13 +39,6 @@ Q11_21 rsqrt(Q21_43 input) {
 	else if ((input >= 8796093022208000)) 	{input = input >> 3; a = -150; b = 349204892981;}
 
 	return (Q11_21)((multiply_Q11_21_by_Q11_21((Q11_21)(input >> 22),a) + b) >> 22);
-}
-
-void update_w() {
-	int i;
-	for (i = 0; i < N; i++) {
-		w_Q6_10[i] = w_next_Q6_10[i];
-	}
 }
 
 /* Piecewise linear approximation of 
@@ -90,93 +87,68 @@ Q11_21 linear_sech2(Q11_21 input) {
 	return (multiply_Q11_21_by_Q11_21(a, input) + b) >> 22;
 }
 
-/* 
-Outputs the magnitude of the
-unmixing vector (reciprocal). 
-*/
-Q11_21 rnorm() {
-	Q21_43 sum = 0;
-	int i;
-
-	for (i = 0; i < N; i++) {
-		sum += ((Q21_43)(multiply_Q6_10_by_Q6_10(w_next_Q6_10[i], w_next_Q6_10[i])))<<22;
-	}
-	return rsqrt(sum);
-}
-
-
-/* 
-Returns 1 if the unmixing vector has converged. 
-*/
-int converged() {
-	Q11_21 dot_product = 0;
-	int i;
-	for (i = 0; i < N; i++) {
-		dot_product += multiply_Q6_10_by_Q6_10(w_Q6_10[i], w_next_Q6_10[i]);
-	}
-
-	if (fabs(fabs(dot_product)-2097152) < EPSILON) { return 1; }
-	else { return 0; }
-}
-
-/*
-Update step in optimization loop.
-*/
-void rotate() {
-	int n,t;
-	for (t = 0; t < T; t++) {
-		product_1[t] = 0;
-		for (n = 0; n < N; n++) {
-			product_1[t] += multiply_Q6_10_by_Q6_10(w_Q6_10[n],whitened_signals[n][t]);
-		}
-
-	}
-
-	for (n = 0; n < N; n++) {
-		product_2[n] = 0; 
-		for (t = 0; t < T; t++) {
-			product_2[n] += multiply_Q6_10_by_Q6_10(whitened_signals[n][t], linear_tanh(product_1[t]));
-		}
-	} 
-
-	sum_1 = 0;
-	for (t = 0; t < T; t++) {
-		sum_1 += linear_sech2(product_1[t]);
-	}
-
-	for (n = 0; n < N; n++) {
-		// divide sum_1 by 2 (shift by 12 instead of 11), multiply w by 2
-		product_3[n] = multiply_Q6_10_by_Q6_10(w_Q6_10[n] << 1, (Q6_10)(sum_1 >> 12));
-	}
-
-	for (n = 0; n < N; n++) {
-		w_next_Q11_21[n] = (product_2[n] - product_3[n]) >> 1;/// N;
-		w_next_Q6_10[n] = (Q6_10)(w_next_Q11_21[n] >> 11);
-	}
-}
-
-/*
-Makes rotation vector into unit vector.
-*/
-void normalize() {
-	int i;
-	Q11_21 w_rnorm = rnorm();
-	for (i = 0; i < N; i++) {
-		w_next_Q21_43[i] = multiply_Q11_21_by_Q11_21(w_rnorm, w_next_Q11_21[i]);
-		w_next_Q6_10[i] = (Q6_10)(w_next_Q21_43[i] >> 33);
-	}
-}
-
 /*
 Main computation.
 Finds one of the vectors of the unmixing matrix.
 */
 void fastica() {
+	int n,t;
+	dot_product = 0; // not converged
+
 	/* rotate, normalize, and repeat */
-	while(!converged()) {
-		update_w();
-		rotate();
-		normalize();
+	while(fabs(fabs(dot_product)-2097152) > EPSILON) {
+		/* update w */
+		for (n = 0; n < N; n++) {
+			w_Q6_10[n] = w_next_Q6_10[n];
+		}
+		
+		/* rotate */
+		for (t = 0; t < T; t++) {
+			product_1[t] = 0;
+			for (n = 0; n < N; n++) {
+				product_1[t] += multiply_Q6_10_by_Q6_10(w_Q6_10[n],whitened_signals[n][t]);
+			}
+
+		}
+
+		for (n = 0; n < N; n++) {
+			product_2[n] = 0; 
+			for (t = 0; t < T; t++) {
+				product_2[n] += multiply_Q6_10_by_Q6_10(whitened_signals[n][t], linear_tanh(product_1[t]));
+			}
+		} 
+
+		sum_1 = 0;
+		for (t = 0; t < T; t++) {
+			sum_1 += linear_sech2(product_1[t]);
+		}
+
+		for (n = 0; n < N; n++) {
+			// divide sum_1 by 2 (shift by 12 instead of 11), multiply w by 2
+			product_3[n] = multiply_Q6_10_by_Q6_10(w_Q6_10[n] << 1, (Q6_10)(sum_1 >> 12));
+		}
+
+		for (n = 0; n < N; n++) {
+			w_next_Q11_21[n] = (product_2[n] - product_3[n]) >> 1;
+			w_next_Q6_10[n] = (Q6_10)(w_next_Q11_21[n] >> 11);
+		}
+
+		/* normalize */
+		sum_2 = 0;
+		for (n = 0; n < N; n++) {
+			sum_2 += ((Q21_43)(multiply_Q6_10_by_Q6_10(w_next_Q6_10[n], w_next_Q6_10[n])))<<22;
+		}
+		w_rnorm = rsqrt(sum_2);
+		for (n = 0; n < N; n++) {
+			w_next_Q21_43[n] = multiply_Q11_21_by_Q11_21(w_rnorm, w_next_Q11_21[n]);
+			w_next_Q6_10[n] = (Q6_10)(w_next_Q21_43[n] >> 33);
+		}
+
+		/* calculate convergence */ 
+		dot_product = 0;
+		for (n = 0; n < N; n++) {
+			dot_product += multiply_Q6_10_by_Q6_10(w_Q6_10[n], w_next_Q6_10[n]);
+		}
 	}
 }
 
