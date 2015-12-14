0dce6fc6424b7bd39e9ce895ddf0641ec7a56981
Loren Lugosch <loren.lugosch@gmail.com>
2015-12-14 15:47:27 -0500

    remove multiple drivers

--- hdl/convergence_check.vhd
+++ hdl/convergence_check.vhd
@@ -28,12 +28,6 @@ ARCHITECTURE arch OF convergence_check IS
 	SIGNAL abs_dot_reg : SIGNED(Q11_21.data_width-1 DOWNTO 0);
 	SIGNAL abs_dot_2 : SIGNED(Q11_21.data_width-1 DOWNTO 0);
 	SIGNAL abs_dot_2_reg : SIGNED(Q11_21.data_width-1 DOWNTO 0);
-	--SIGNAL plus_dot : SIGNED(Q11_21.data_width-1 DOWNTO 0);
-	--SIGNAL minus_dot : SIGNED(Q11_21.data_width-1 DOWNTO 0);
-	--SIGNAL plus_dot_reg : SIGNED(Q11_21.data_width-1 DOWNTO 0);
-	--SIGNAL minus_dot_reg : SIGNED(Q11_21.data_width-1 DOWNTO 0);
-	--SIGNAL plus_diff : SIGNED(Q11_21.data_width-1 DOWNTO 0);
-	--SIGNAL minus_diff : SIGNED(Q11_21.data_width-1 DOWNTO 0);
 
 	SIGNAL valid_w_reg_1 : STD_LOGIC;
 	SIGNAL valid_w_reg_2 : STD_LOGIC;
@@ -48,9 +42,7 @@ ARCHITECTURE arch OF convergence_check IS
 BEGIN
 	w_next_Q6_10_out <= w_next_Q6_10_in; -- register this!
 
-	dot <= (w_Q6_10_reg(0)*w_next_Q6_10_in(0) + w_Q6_10_reg(1)*w_next_Q6_10_in(1)) sll 1; 
-
-	PROCESS(w_next_Q6_10_in, w_Q6_10, dot, dot_reg, abs_dot, abs_dot_reg, abs_dot_2, abs_dot_2_reg, valid_w_reg_5)
+	PROCESS(w_next_Q6_10_in, w_Q6_10_reg, dot, dot_reg, abs_dot, abs_dot_reg, abs_dot_2, abs_dot_2_reg, valid_w_reg_5)
 	BEGIN
 		dot <= (w_Q6_10_reg(0)*w_next_Q6_10_in(0) + w_Q6_10_reg(1)*w_next_Q6_10_in(1)) sll 1; 
 		IF (dot < 0) THEN
@@ -73,50 +65,12 @@ BEGIN
 	END PROCESS;
 
 
-	--plus_dot <= dot_reg;
-	--minus_dot <= (NOT dot_reg) + 1; 
-	--plus_diff <= plus_dot_reg - "00000000001000000000000000000000";
-	--minus_diff <= minus_dot_reg - "00000000001000000000000000000000";
-
-	--PROCESS(plus_diff, minus_diff, valid_w_reg_3)
-	--BEGIN
-	--	IF ((plus_diff < EPSILON) OR (minus_diff > EPSILON)) AND (valid_w_reg_3 = '1') THEN
-	--		converged <= '1';
-	--	ELSE 
-	--		converged <= '0';
-	--	END IF;
-	--END PROCESS;
-
-	--PROCESS(w_next_Q6_10_in, w_Q6_10, dot, dot_reg, abs_dot, abs_dot_reg, abs_dot_2, abs_dot_2_reg, valid_w)
-	--BEGIN
-		--dot <= (w_Q6_10(0)*w_next_Q6_10_in(0) + w_Q6_10(1)*w_next_Q6_10_in(1)) sll 1; 
-		--IF (dot < 0) THEN
-		--	abs_dot <= (NOT dot_reg) + 1; -- multiply by -1 in two's complement
-		--ELSE
-		--	abs_dot <= dot_reg;
-		--END IF;
-
-		--IF ((abs_dot - "00000000001000000000000000000000") < "00000000000000000000000000000000") THEN
-		--	abs_dot_2 <= "00000000001000000000000000000000" - abs_dot_reg;
-		--ELSE
-		--	abs_dot_2 <= abs_dot_reg - "00000000001000000000000000000000";
-		--END IF;
-
-		--IF (abs_dot_2_reg < EPSILON) AND (valid_w = '1') THEN
-		--	converged <= '1';
-		--ELSE 
-		--	converged <= '0';
-		--END IF;
-	--END PROCESS;
-
 	PROCESS(clock, reset)
 	BEGIN
 		IF (reset = '1') THEN
 			dot_reg <= (OTHERS => '0');
 			abs_dot_reg <= (OTHERS => '0');
 			abs_dot_2_reg <= (OTHERS => '0');
-			--plus_dot_reg <= (OTHERS => '0');
-			--minus_dot_reg <= (OTHERS => '0');
 			valid_w_reg_1 <= '0';
 			valid_w_reg_2 <= '0';
 			valid_w_reg_3 <= '0';
@@ -128,8 +82,6 @@ BEGIN
 			dot_reg <= dot;
 			abs_dot_reg <= abs_dot;
 			abs_dot_2_reg <= abs_dot_2;
-			--plus_dot_reg <= plus_dot;
-			--minus_dot_reg <= minus_dot;
 			valid_w_reg_1 <= valid_w;
 			valid_w_reg_2 <= valid_w_reg_1;
 			valid_w_reg_3 <= valid_w_reg_2;
